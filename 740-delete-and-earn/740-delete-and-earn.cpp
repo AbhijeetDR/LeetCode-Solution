4ba6e78286c280eb9120da811e7f3edfc0b585fd
Abhijeet Raotole <81011229+AbhijeetDR@users.noreply.github.com>
2022-08-20 19:46:53 +0530

    Time: 19 ms (68.71%), Space: 16.5 MB (14.83%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int> freq(1e4+1, 0);
+        vector<int> dp(1e4+1, 0);
+        for(int i =0; i < n; i++){
+            freq[nums[i]]++;
+        }
+        
+        //2 2 3 3 3 4 5
+        //0 1 2 3 4 5 6
+        //0 0 4 9 8 16
+        
+        //8 1 4 9 1 3 5 9 4 10
+        
+        //1 1 3 4 4 5 8 9 9 10
+        //0 1 2 3 4 5 6 7 8 9 10
+        //0 2 2 5 10
+        
+        int maxi = INT_MIN;
+        dp[0] = 0, dp[1] = freq[1];
+        for(int i = 2 ; i < 1e4+1; i++){
+            dp[i] = max(i*freq[i] + dp[i-2], dp[i-1]);
+            maxi = max(maxi, dp[i]);
+        }
+        
+        return maxi;
+    }
+};
