6f9379cd6a8b0aeb9e4179eaa1712d9ff5fde645
Abhijeet Raotole <81011229+AbhijeetDR@users.noreply.github.com>
2022-08-20 19:00:24 +0530

    Time: 2 ms (58.52%), Space: 7.6 MB (77.09%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -4,13 +4,19 @@ public:
         //dp[i] = max money till i houses
         int n = nums.size();
         if(n == 1) return nums[0];
-        vector<int> dp(n, 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        // vector<int> dp(n, 0);
+        // dp[0] = nums[0];
+        // dp[1] = max(nums[0], nums[1]);
+        
+        int p2 = nums[0], p1 =max(p2, nums[1]);
+        int cur = max(p1, p2);
         for(int i = 2; i < n; i++){
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
+            // dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
+            cur = max(p1, p2 + nums[i]);
+            p2 = p1;
+            p1 = cur;
         }
-        return dp[n-1];
+        return cur;
         
     }
 };
