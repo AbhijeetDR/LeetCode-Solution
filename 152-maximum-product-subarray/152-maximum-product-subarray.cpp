e6e8b292e35f6520f5840181c11968127b3c5bd0
Abhijeet Raotole <81011229+AbhijeetDR@users.noreply.github.com>
2022-08-27 00:43:57 +0530

    Time: 8 ms (78.34%), Space: 13.8 MB (25.90%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -5,14 +5,21 @@ public:
         
         //1 3 -2 6 -1 2 -1
         //dp[maxi][mini]
-        
-        vector<vector<int>> dp(n, vector<int> (2, 1));
-        dp[0][0] = dp[0][1] = v[0];
+        int cur_max = 0;
+        int cur_min = 0;
+        int pre_max =v[0];
+        int pre_min =v[0];
+        // vector<vector<int>> dp(n, vector<int> (2, 1));
+        // dp[0][0] = dp[0][1] = v[0];
         int ans = v[0];
         for(int i= 1; i <n;i++){
-            dp[i][0] = max({v[i] * dp[i-1][0], v[i] * dp[i-1][1], v[i]});
-            dp[i][1] = min({v[i]*dp[i-1][0], v[i] * dp[i-1][1], v[i]});
-            ans = max(ans, dp[i][0]);
+            // dp[i][0] = max({v[i] * dp[i-1][0], v[i] * dp[i-1][1], v[i]});
+            cur_max = max({v[i] * pre_max, v[i] * pre_min, v[i]});
+            // dp[i][1] = min({v[i]*dp[i-1][0], v[i] * dp[i-1][1], v[i]});
+            cur_min = min({v[i]*pre_max, v[i]*pre_min, v[i]});
+            ans = max(ans, cur_max);
+            pre_max = cur_max;
+            pre_min = cur_min;
         }
         
         return ans;
