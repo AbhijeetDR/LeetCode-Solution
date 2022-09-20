fd10874508a14333ef52f013bc79b62461ddffc9
Abhijeet Raotole <81011229+AbhijeetDR@users.noreply.github.com>
2022-09-20 19:58:35 +0530

    Time: 672 ms (25.93%), Space: 11.9 MB (92.72%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -4,21 +4,18 @@ public:
         int n = nums1.size();
         int m = nums2.size();
         int ans =0 ;
-        // vector<vector<int>>dp(n+1, vector<int>(m+1,0));
-        vector<int>prev(m+1, 0);
+        vector<int>dp(m+1, 0);
         for(int i = 1; i <= n; i++){
             for(int j = m; j >= 1; j--){
                 if(nums1[i-1] == nums2[j-1]){
-                    prev[j] = prev[j-1] + 1;
-                    ans = max(prev[j], ans);
+                    dp[j] = dp[j-1] + 1;
+                    ans = max(dp[j], ans);
                 }
                 else{
-                    prev[j] = 0;
+                    dp[j] = 0;
                 }
             }
-            // prev = cur;
         }
         return ans;
-        
     }
 };
