cd8f55f25080dd3d51953884e3c864689a0fa535
Abhijeet Raotole <81011229+AbhijeetDR@users.noreply.github.com>
2022-09-23 23:19:42 +0530

    Time: 533 ms (27.17%), Space: 63.1 MB (16.30%) - LeetHub

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    
+public:
+    long long int mod;
+    Solution(){
+        mod =(int)(1e9 + 7);
+    }
+    
+    long long int binpow(long long int a, long long int b){
+        if(b == 0) return 1;
+        if(b == 1) return a;
+        int res = binpow(a, b/2);
+        if(b%2){
+            return (res*(res*a)%mod)%mod;
+        }
+        return (res*res)%mod;
+    }
+    
+    int concatenatedBinary(int n) {
+        vector<long long int> dp(n+1);
+        dp[1] = 1;
+        for(int i= 2; i <= n; i++){
+            dp[i] = ((dp[i-1]*binpow(2, log2(i) + 1))%mod + i)%mod;
+        }
+        return dp[n];
+    }
+};
